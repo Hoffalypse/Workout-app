8289ca52c683808da1a57c05d45f077162baed3e
kumenger <44558777+kumenger@users.noreply.github.com>
2023-05-29 15:10:01 -0600

    prfile and serach componet

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Profile = () => {
+const Search = () => {
   return (
     <div>
-      Profile
+      Serach
     </div>
   )
 }
 
-export default Profile
+export default Search
