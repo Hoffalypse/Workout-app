a9d7283a3b28adcd0c3909dabf8b8341ad5151cf
John <johnwilday@yahoo.com>
2023-05-29 23:15:43 -0600

    Built and styled homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import Navbar from '../components/Navbar' 
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div style={{ backgroundImage: 'linear-gradient(yellow, red)', width: '100%', height: '100vh'}}>
+      <Navbar/>
+      <div style={{ textAlign: 'center' }}>
+<h1 style={{ marginTop: '50px' }} >Your Ultimate Workout Builder</h1>
+
+<p style={{ marginRight: 'auto', marginLeft: 'auto', width: '55%' }}>Search through over 1,300 exercises and create a custom workout routine based on the areas you want to target or the home-exercise equipment you have. Everything at Workout Wizard is free. Setup your account or login to access all the exercise information. </p>
+
+</div>
+<div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '30px' }}>
+  <img className='img-home' src='http://d205bpvrqc9yn1.cloudfront.net/2333.gif' alt='arm slingers hanging straight legs'/>
+  <img className='img-home' src='http://d205bpvrqc9yn1.cloudfront.net/3361.gif' alt='skater hops'/>
+  <img className='img-home' src='http://d205bpvrqc9yn1.cloudfront.net/0301.gif' alt='dumbbell decline bench press'/>
+</div>
+
+
+{/* <p style={{ textAlign: 'center', marginTop: '50px' }}>&#169; Workout Wizard 2023</p> */}
+
     </div>
   )
 }
