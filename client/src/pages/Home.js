15bacc29a7e07ba15b8bb173a52e7f736a1889c6
kumenger <44558777+kumenger@users.noreply.github.com>
2023-06-06 02:23:35 -0600

    homepage responsive layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,12 +2,12 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 
 const Home = () => {
-  return (<>
+  return (<div className=''>
     <Navbar currentPage="home"/>
-    <div style={{ backgroundImage: 'linear-gradient(red, yellow)', width: '100%', height: '100vh', marginTop: '0px', 
+    <div className='row' style={{ backgroundImage: 'linear-gradient(red, yellow)', width: '100%', height: '100vh', marginTop: '0px', 
     paddingTop: '0px' }}>
-      <br/><br/>
-      <div style={{ textAlign: 'center' }}>
+ 
+      <div className='col-12' style={{ textAlign: 'center' }}>
         <h1 
         // style={{ marginTop: '50px' }}
         >The Ultimate Custom Workout Builder</h1>
@@ -15,17 +15,17 @@ const Home = () => {
         <p style={{ marginRight: 'auto', marginLeft: 'auto', width: '55%' }}>Search through over 1,300 exercises and create a custom workout routine based on the areas you want to target or the home-exercise equipment you have. Everything at Workout Wizard is free. Setup your account or login to access all the exercise information. </p>
 
       </div>
-      <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '30px' }}>
-        <img className='img-home' src='http://d205bpvrqc9yn1.cloudfront.net/2333.gif' alt='arm slingers hanging straight legs' />
-        <img className='img-home' src='http://d205bpvrqc9yn1.cloudfront.net/3361.gif' alt='skater hops' />
-        <img className='img-home' src='http://d205bpvrqc9yn1.cloudfront.net/0301.gif' alt='dumbbell decline bench press' />
-      </div>
+    
+      <div className='col-xs-12 col-s-6 col-md-6 col-l-4 col-xl-4' style={{padding:"30px"}} ><img className='img-home img-responsive' src='http://d205bpvrqc9yn1.cloudfront.net/2333.gif' alt='arm slingers hanging straight legs' /></div>  
+      <div className='col-xs-12 col-s-6 col-md-6 col-l-4 col-xl-4'style={{padding:"30px"}}> <img className='img-home' src='http://d205bpvrqc9yn1.cloudfront.net/3361.gif' alt='skater hops' /></div>   
+      <div className='col-xs-12 col-s-6 col-md-6 col-l-4 col-xl-4' style={{padding:"30px"}}> <img className='img-home' src='http://d205bpvrqc9yn1.cloudfront.net/0301.gif' alt='dumbbell decline bench press' /></div>    
+
 
 
       {/* <p style={{ textAlign: 'center', marginTop: '50px' }}>&#169; Workout Wizard 2023</p> */}
 
     </div>
-  </>)
+  </div>)
 }
 
 export default Home
