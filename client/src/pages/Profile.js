8558a383baed5f2f564e594f75c836af3229e780
John <johnwilday@yahoo.com>
2023-06-06 20:19:53 -0600

    Finished rendering all exercises by category on Profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,12 +3,23 @@ import { useMutation, useQuery } from "@apollo/client";
 import { QUERY_EXE } from "../utils/queries";
 import SearchCard from "../components/SearchCard";
 import Navbar from "../components/Navbar";
-import { capitalize } from '../utils/helper';
+import { capitalize } from "../utils/helper";
+import ProfileCard from "../components/ProfileCard";
 
 const Profile = () => {
   const { data } = useQuery(QUERY_EXE);
   const [allExercises, setAllExercises] = useState([]);
   const [back, setBack] = useState([]);
+  const [neck, setNeck] = useState([]);
+  const [chest, setChest] = useState([]);
+  const [shoulders, setShoulders] = useState([]);
+  const [upperArms, setUpperArms] = useState([]);
+  const [lowerArms, setLowerArms] = useState([]);
+  const [waist, setWaist] = useState([]);
+  const [upperLegs, setUpperLegs] = useState([]);
+  const [lowerLegs, setLowerLegs] = useState([]);
+  const [cardio, setCardio] = useState([]);
+  
 
   useEffect(() => {
     const all = async () => {
@@ -16,7 +27,6 @@ const Profile = () => {
         const dataHere = await data?.getUser?.savedExercise;
 
         setAllExercises(dataHere);
-    
       }
     };
 
@@ -24,62 +34,84 @@ const Profile = () => {
   }, [data]);
 
   useEffect(() => {
-    const filterBackExercises = async () => {
+    const filterAllExercises = async () => {
       if (allExercises) {
         const backEx = await allExercises.filter(
           (ex) => ex.bodyPart === "back"
         );
+        const neckEx = await allExercises.filter(
+          (ex) => ex.bodyPart === "neck"
+        );
+        const chestEx = await allExercises.filter(
+          (ex) => ex.bodyPart === "chest"
+        );
+        const shouldersEx = await allExercises.filter(
+          (ex) => ex.bodyPart === "shoulders"
+        );
+        const upperArmsEx = await allExercises.filter(
+          (ex) => ex.bodyPart === "upper arms"
+        );
+        const lowerArmsEx = await allExercises.filter(
+          (ex) => ex.bodyPart === "lower arms"
+        );
+        
+        const waistEx = await allExercises.filter(
+          (ex) => ex.bodyPart === "waist"
+        );
+        const upperLegsEx = await allExercises.filter(
+          (ex) => ex.bodyPart === "upper legs"
+        );
+        const lowerLegsEx = await allExercises.filter(
+          (ex) => ex.bodyPart === "lower legs"
+        );
+        const cardioEx = await allExercises.filter(
+          (ex) => ex.bodyPart === "cardio"
+        );
         setBack(backEx);
-      
+        setNeck(neckEx);
+        setChest(chestEx);
+        setShoulders(shouldersEx)
+        setUpperArms(upperArmsEx)
+        setLowerArms(lowerArmsEx)
+        setWaist(waistEx)
+        setUpperLegs(upperLegsEx)
+        setLowerLegs(lowerLegsEx)
+        setCardio(cardioEx)
       }
     };
 
-    filterBackExercises();
-  }, [allExercises]);
+    filterAllExercises();
 
-  console.log(back);
+   
+  }, [allExercises]);
 
   return (
     <div>
-      <Navbar/>
-      {back ? (
-        <div>
-          <h1>Back</h1>
-          <div style={{ display: "flex", flexWrap: "wrap" }}>
-            {back.map((ex) => (
-              <div
-                style={{
-                  // maxWidth: "340px",
-                  textAlign: "center",
-                  border: "4px solid black",
-                  margin: "5px",
-                  borderRadius: "10px",
-                }}
-              >
-                <img src={ex.gifUrl} />
-                <div
-                  style={{
-                    backgroundImage: "linear-gradient(yellow, red)",
-                    height: "24vh",
-                  }}
-                >
-                  <p style={{ fontWeight: "bold", maxWidth: "350px" }}>
-                    {capitalize(ex.name)}
-                  </p>
-                  <p>Target: {capitalize(ex.target)}</p>
-                  <p style={{ marginTop: "-20px" }}>
-                    Equipment: {capitalize(ex.equipment)}
-                  </p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      ) : (
-        ""
-      )}
+      <Navbar />
+      {back.length !== 0 ? <ProfileCard exPart="Back" exerciseData={back} /> : ""}
+
+      {neck.length !== 0 ? <ProfileCard exPart="Neck" exerciseData={neck} /> : ""}
+
+      {chest.length !== 0 ? <ProfileCard exPart="Chest" exerciseData={chest} /> : ""}
+
+      {shoulders.length !== 0 ? <ProfileCard exPart="Shoulders" exerciseData={shoulders} /> : ""}
+
+      {upperArms.length !== 0 ? <ProfileCard exPart="Upper Arms" exerciseData={upperArms} /> : ""}
+
+      {lowerArms.length !== 0 ? <ProfileCard exPart="Lower Arms" exerciseData={lowerArms} /> : ""}
+      
+      {waist.length !== 0 ? <ProfileCard exPart="Waist" exerciseData={waist} /> : ""}
+
+      {upperLegs.length !== 0 ? <ProfileCard exPart="Upper Legs" exerciseData={upperLegs} /> : ""}
+
+      {lowerLegs.length !== 0 ? <ProfileCard exPart="Lower Legs" exerciseData={lowerLegs} /> : ""}
+
+      {cardio.length !== 0 ? <ProfileCard exPart="Cardio" exerciseData={cardio} /> : ""}
+
+
     </div>
   );
 };
 
 export default Profile;
+
